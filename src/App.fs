a3b36163bea15e393a8005da0cbab19885a41a36
Tre <wseymour@envysion.com>
2018-03-06 16:51:43 -0700

    JQuery manually typed to be a fp-ish module.
    Dont forget to use CMD+SHIFT+B to build the project in VSCode.

--- src/App.fs
+++ src/App.fs
@@ -52,3 +52,18 @@ type IJQuery = interface end
 //    |> JQuery.addClass "fancy-class"
 //    |> ignore
 // )
+
+
+module JQuery = 
+
+  [<Emit("window['$']($0)")>]
+  let ready (handler: unit -> unit) : unit = jsNative
+  
+  [<Emit("$2.css($0, $1)")>]
+  let css (prop: string) (value: string) (el: IJQuery) : IJQuery = jsNative
+  
+  [<Emit("$1.addClass($0)")>]
+  let addClass (className: string) (el: IJQuery) : IJQuery = jsNative
+  
+  [<Emit("$1.click($0)")>]
+  let click (handler: obj -> unit)  (el: IJQuery) : IJQuery = jsNative
